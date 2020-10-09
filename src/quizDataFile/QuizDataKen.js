008d2fb527f5d9fdb472e16555da75baed013620
Ken Tauchi <kentauchica@gmail.com>
2020-10-08 23:30:42 -0700

    modify quiz data

--- src/quizDataFile/QuizDataKen.js
+++ src/quizDataFile/QuizDataKen.js
@@ -1,19 +1,19 @@
 const QuizDataKen = [
   {
     id: 0,
-    question: "What year was I born?",
+    question: "What year was he born?",
     option: [1990, 1991],
     answer: 1991,
   },
   {
     id: 1,
-    question: "What city was I born in?",
+    question: "What city was he born in?",
     option: ["Tokyo", "Osaka"],
     answer: "Osaka",
   },
   {
     id: 2,
-    question: "How many countries have I traveled to?",
+    question: "How many countries has he traveled to?",
     option: [8, 4],
     answer: 8,
   },
@@ -22,6 +22,12 @@ const QuizDataKen = [
     question: "What sports has he got a MVP?",
     option: ["Karate", "Football"],
     answer: "Football",
+  },
+  {
+    id: 4,
+    question: "What is his nickname?",
+    option: ["Kenny", "Totty"],
+    answer: "Kenny",
   },
 ];
 
